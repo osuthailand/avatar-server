4f3303dee04c2e494b57a4d9dfb983ed68e011c5
Simon <45692977+denmarkistrash@users.noreply.github.com>
2020-06-17 23:38:23 +0200

    .

--- avatarserver.py
+++ avatarserver.py
@@ -16,48 +16,29 @@ curryear = int(datetime.now().year)
 today = datetime.date(datetime(curryear, int(datetime.now().month), int(datetime.now().day)))
 peppyday = datetime.date(datetime(curryear, 4, 20))
 
-@app.route("/status")
-def serverStatus():
-	return jsonify({
-		"response" : 200,
-		"status" : 1
-	})
-
 @app.route("/<int:uid>")
 def serveAvatar(uid):
 	# Check if avatar exists
 	peppy = random.randrange(1,16)
-	if peppyday != today:
-		if os.path.isfile("{}/{}.png".format(avatar_dir, uid)):
-			avatarid = uid
-		else:
-			avatarid = -1
-
-	returnthi = ""
 	if peppyday == today:
-		returnthi = send_file("{}/{}.png".format(peppy_dir, peppy))
-	elif peppyday != today:
-		returnthi = send_file("{}/{}.png".format(avatar_dir, avatarid))
-	# Serve actual avatar or default one
-	return returnthi
+		return send_file("{}/{}.png".format(peppy_dir, peppy))
 
-@app.route("/<int:uid>/peppyday")
-def serveAvatarPPYday(uid):
-	# Check if avatar exists
-	peppy = random.randrange(1,16)
-	
-	returnthi = send_file("{}/{}.png".format(peppy_dir, peppy))
+	if os.path.isfile("{}/{}.png".format(avatar_dir, uid)):
+		avatarid = uid
+	else:
+		avatarid = -1
+
 	# Serve actual avatar or default one
-	return returnthi
+	return send_file("{}/{}.png".format(avatar_dir, avatarid))
 
 @app.errorhandler(404)
 def page_not_found(error):
 	peppy = random.randrange(1,16)
 	if peppyday == today:
 		return send_file("{}/{}.png".format(peppy_dir, peppy))
-	elif peppyday != today:
-		return send_file("{}/-1.png".format(avatar_dir))
+
+	return send_file("{}/-1.png".format(avatar_dir))
 
 	
 # Run the server
-app.run(host="0.0.0.0", debug=True, port=5000)
+app.run(host="0.0.0.0", port=5000)
